02c719126eefe2bb71bc0cf1834c0281c47ad4c7
Hillelg1 <hillelgersten@gmail.com>
2025-12-10 12:30:27 -0500

    started commentary but going to add a switch statement for cleaner code

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-
+//this is for our mksnodes to identify what it is they are holding
 enum Label {
 	op,
 	int_const,
@@ -16,8 +16,10 @@ enum Label {
 	float_const,
 };
 
+//this is for our mksnode to label our actual and computed types
 enum EntryType {TYPE_UNSET = -1,INTEGER = 0,FLOATING = 1};
 
+//what a row in the table should look like;
 struct TableEntry {
 	bool is_identifier;
 	EntryType type;
@@ -25,9 +27,11 @@ struct TableEntry {
 	string name;
 };
 
+//our actual table
 unordered_map<string, TableEntry> lookupTable;
 
-
+//the nodes that we will build our tree with , we label what they are, their content will be what they hold, actual type is what they
+// really are and computed is what we have to change them to, if a node is lhs then we know its a part of an assign
 struct MkSnode {
 	Label label;
 	EntryType actualType;
@@ -40,6 +44,7 @@ struct MkSnode {
 		: label(lb), content(std::move(cn)), actualType(TYPE_UNSET), computedType(TYPE_UNSET), left_child(left), right_child(right), is_lhs(false) {}
 };
 
+//this is for each element in our ASM we will either hold an int, a float, or an ident (for when its lhs)
 struct StackEntry {
 	union {
 		int i_val;
@@ -47,7 +52,7 @@ struct StackEntry {
 	};
 	string ident;
 };
-
+//data structure for running our under the hood computations for each ilcg instruction
 struct ASMStack {
 	private:
 		StackEntry data[100] = {};
@@ -67,6 +72,7 @@ struct ASMStack {
 	}
 };
 
+// this is for when we have things like y=l=j etc, we need a way to chain these together before entering the rhs so hold them in a stack
 struct AssignStack{
 	private:
 		MkSnode* data[100] ={};
@@ -115,17 +121,19 @@ void addChar();
 void getChar();
 void getNonBlank();
 int lex();
-void program(ifstream &infile);
 void print(const unordered_map<string, TableEntry>& table);
+
+//syntax analysis functions
+void program(ifstream &infile);
 MkSnode* assign_list();
 void declare_list(bool isInt);
 TableEntry declare(bool isInt);
-MkSnode* assign();
 MkSnode* term();
 MkSnode* expr();
 MkSnode* factor();
 MkSnode* error();
 
+//ilcg instructions
 void ftoi();
 void itof();
 void iadd();
@@ -144,6 +152,7 @@ void doOp(MkSnode* root);
 /* Token codes */
 #define INTLIT 10
 #define IDENT 11
+//we can now have floating point nums
 #define FLOATLIT 12
 #define ASSIGNOP 20
 #define ADDOP 21
@@ -160,10 +169,12 @@ void doOp(MkSnode* root);
 /******************************************************/
 
 int main() {
+	//declare testcases
 	string testCases[2] = {"front.in1","front.in2"};
-
+	// loop through each test case
 	for (const auto& test: testCases) {
 		cout<< "ENTERING TESTCASE: "<< test << endl;
+		//populate table with initial float and int values
 		TableEntry intEntry;
 		TableEntry floatEntry;
 		floatEntry.is_identifier = false;
@@ -178,8 +189,10 @@ int main() {
 			cerr<<"cannot open: "<<test<<endl;
 		}
 		else {
+			//loop through the contents of each test case
 			program(infile);
 		}
+		//clear stack and table
 		ASM.clear();
 		lookupTable.clear();
 	}
@@ -217,6 +230,7 @@ int lookup(char ch) {
 			addChar();
 			nextToken = ASSIGNOP;
 			break;
+		// now we can have a , for a declare list
 		case ',':
 			nextToken = COMMA;
 			break;
@@ -275,28 +289,35 @@ int lex() {
 					addChar();
 					getChar();
 				}
+				//find out if we are dealing with an ident int or float, look into the lexeme to see
 				if (strcmp("int",lexeme)==0)nextToken = INT;
 				else if (strcmp("float",lexeme)==0)nextToken = FLOAT;
 				else nextToken = IDENT;
 				break;
 		case DIGIT: {
+			//default is int
 			bool isFloat = false;
 			addChar();
 			getChar();
+			//build out the initial part of float or the entire digit
 			while (charClass == DIGIT) {
 				addChar();
 				getChar();
 			}
+			//weve encountered a float
 			if (nextChar == '.') {
 				isFloat = true;
+				//consume the .
 				addChar();
 				getChar();
+				//this means we have somethign like 1.
 				if (charClass != DIGIT){error(); nextToken = EOL;break;}
 				while (charClass == DIGIT) {
 					addChar();
 					getChar();
 				}
 			}
+			// for the case of 1e or 1.1E etc
 			if (nextChar == 'E' || nextChar == 'e') {
 				isFloat = true;
 				addChar();
@@ -328,12 +349,14 @@ int lex() {
 	return nextToken;
 } /* End of function lex */
 
-
+//program -> program{declare_list}|program{assign_list } we said for the sake of the project that we will assume that we always have a declare list to start
 void program(ifstream &infile) {
 	string line;
 
 	while (getline(infile, line)) {
+		//loop line by line in the testcase
 		if (line.empty())continue;
+		//reset variables
 		currentLine = line;
 		idx = 0;
 		nextChar = 0;
@@ -341,22 +364,28 @@ void program(ifstream &infile) {
 		lexLen = 0;
 		getChar();
 		lex();
+		//we are declaring a list of integers or floats
 		if (nextToken == INT){declare_list(true);}
 		else if (nextToken == FLOAT){declare_list(false);}
+		//if we start with ident we know its an assign list
 		else if (nextToken == IDENT) {
 			assign_list();
 		}
 		else error();
 	}
 }
+
+//declare_list -> (int|float) <ident> = [<expr>] {,<ident> = [<expr>]
 void declare_list(const bool isInt) {
 	if (nextToken != INT && nextToken != FLOAT) {
 		error();
 		nextToken = EOF;
 	}
 	lex(); // consume int/float
+	//initial call;
 	TableEntry ret = declare(isInt);
 	lookupTable[ret.name] = ret;
+	//encountering a , means we have another var to declare
 	while (nextToken == COMMA) {
 		lex(); // consume ,
 		TableEntry next = declare(isInt);
@@ -364,10 +393,12 @@ void declare_list(const bool isInt) {
 	}
 }
 
+// a singular declare <ident> = const
 TableEntry declare(const bool isInt) {
 	if (nextToken != IDENT) {
 		return {};
 	}
+	//table row to populate table with
 	auto ret = TableEntry();
 	ret.is_identifier = true;
 	ret.name = lexeme;
@@ -378,10 +409,11 @@ TableEntry declare(const bool isInt) {
 	lex(); // consume ident
 
 	if (nextToken != ASSIGNOP) {
-		return ret; //just ident,
+		return ret; //just ident, no ident =
 	}
 	lex(); // consume =
 
+	//assign our values to the table entry
 	if (isInt) {
 		ret.value.i_val = nextToken == INTLIT? stoi(lexeme) : static_cast<int> (stof(lexeme));
 	}
@@ -392,15 +424,19 @@ TableEntry declare(const bool isInt) {
 	return ret;
 }
 
-
+// these are my semantic analysis functions
+//for assigning actual types we need to do a dfs and assign based on child
 MkSnode* actualType(MkSnode* root) {
 	if (!root) return nullptr;
+	//this means we need to get the actual type based on the node itself since its a leaf
 	if (!root->right_child && !root->left_child) {
+		//if its an identifier we need to look it up in the table
 		if (root-> label == identifier) {
 			const auto& entry = lookupTable[root->content];
 			root-> actualType = entry.type;
 			return root;
 		}
+		//float or int means actual type is float or int
 		if (root-> label == float_const) {
 			root-> actualType = FLOATING;
 			return root;
@@ -410,31 +446,41 @@ MkSnode* actualType(MkSnode* root) {
 			return root;
 		}
 	}
+	//dfs if we didnt hit a leaf node
 	MkSnode* lChild = actualType(root->left_child);
 	MkSnode* rChild = actualType(root->right_child);
 	if (!lChild && !rChild) return nullptr;
 
+	//assign the typing based on lchild for the edge case of having =
 	if (root->content == "=") {
 		root-> actualType = lChild->actualType;
 		return root;
 	}
+	//case 2 if either are a float then the operations actual type must be float
 	if (rChild-> actualType == FLOATING || lChild-> actualType == FLOATING) {
 		root-> actualType = FLOATING;
 		return root;
 	}
+	//otherwise we have an integer if both are actual ints
 	root->actualType = INTEGER;
 	return root;
 }
 
+//this is the pre order search through the tree to label our expected types
 void computedTypes(MkSnode* root) {
+	// edge base case
 	if (!root) return;
+	//real base case, since we are doing pre order, theres nothing to do by the children
 	if (!root->right_child && !root->left_child) {
 		return;
 	}
+	//label our lhs in this case since we have an = we also make the right childs
+	//computed type be based on lchild actual and not parent
 	if (root-> content == "=" && root->left_child && root->right_child) {
 		root-> right_child-> computedType = root->left_child->actualType;
 		root->left_child -> is_lhs = true;
 	}
+	//otherwise just assign the childrens computed type based on parents actual type
 	else {
 		if (root-> right_child)root->right_child->computedType = root->actualType;
 		if (root->left_child)root->left_child->computedType = root->actualType;
@@ -443,29 +489,37 @@ void computedTypes(MkSnode* root) {
 	computedTypes(root->right_child);
 }
 
+//this ends the semantic analysis functions we are now entering the ilcg part of compiler
+//for generating ilcg we do a dfs search and then generate code based on what we return
 void ilcg(MkSnode* root) {
+	//base case nullptr
 	if (!root) return;
 	ilcg(root->left_child);
 	ilcg(root->right_child);
 	StackEntry entry = StackEntry();
+	//if we are on the lhs of an expr we do the special case of populating the stack with the adress
 	if (root-> is_lhs) {
 		entry.ident = root-> content;
 		ASM.push(entry);
 		cout <<"pushaddr("<<root->content<<")"<<endl;
 		return;
 	}
+	//if were not lhs but we are by an identifier we have to populate the stack with the values we get from the table
 	if (root-> label == identifier) {
 		TableEntry tableEntry = lookupTable[root->content];
+		//populate with an int based on table entry
 		if (root-> actualType == INTEGER) {
 			int num = tableEntry.value.i_val;
 			entry.i_val = num;
 			ASM.push(entry);
 			cout << "push("<<root->content<<")" << endl;
+			//if computed != actual we have to convert
 			if (root-> computedType == FLOATING) {
 				itof();
 			}
 			return;
 		}
+		//same logic as int but inverted for float
 		if (root-> actualType == FLOATING) {
 			float num = tableEntry.value.f_val;
 			entry.f_val = num;
@@ -477,6 +531,8 @@ void ilcg(MkSnode* root) {
 		}
 		return;
 	}
+	//otherwise if we are dealing with a float const we just push the value that the node itself holds
+	//we dont lookup in the table because theres no entry for that
 	if (root-> label == float_const) {
 		const float fnum = stof(root->content);
 		cout << "push("<<fnum<<")" << endl;
@@ -484,18 +540,21 @@ void ilcg(MkSnode* root) {
 		ASM.push(entry);
 		return;
 	}
+	//same logic as float but we do have to coerce the int to float if needed since float takes priority
+	//in our arithmetic typing
 	if (root-> label == int_const) {
 		const int inum = stoi(root->content);
 		cout << "push("<<inum<<")" << endl;
 		entry.i_val = inum;
 		ASM.push(entry);
+		if (root-> computedType == FLOATING) {
+			itof();
+		}
 		return;
 	}
 	if (root-> label == op) {
 		doOp(root);
 	}
-
-
 }
 
 void doOp(MkSnode* root) {
@@ -571,7 +630,6 @@ void doOp(MkSnode* root) {
 		if (root-> computedType == FLOATING && root-> actualType == INTEGER) {itof();}
 		if (root-> computedType == INTEGER && root-> actualType == FLOATING) {ftoi();}
 	}
-
 }
 void ftoi() {
 	cout << "fToi"<<endl;
